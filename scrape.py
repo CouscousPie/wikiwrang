513201988cadd3a7b29869a3b2eb5bbe3303004d
Quentin <quentin.czernin@gmail.com>
2022-11-26 15:29:17 +0100

    insert scraping code

---
+++ scrape.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import requests
+import lxml
+from bs4 import BeautifulSoup
+
+wikiurl = "https://de.wikipedia.org/wiki/Liste_der_gr%C3%B6ssten_Unternehmen_in_der_Schweiz"
+
+
+table_class="wikitable sortable jquery-tablesorter"
+response=requests.get(wikiurl)
+print(response.status_code)
+
+
+soup = BeautifulSoup(response.text, 'html.parser')
+indiatable=soup.find('table',{'class':"wikitable"})
+
+df=pd.read_html(str(indiatable))
+# convert list to dataframe
+df=pd.DataFrame(df[0])
+print(df.head())
